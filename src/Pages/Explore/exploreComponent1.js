001d9a870dacce808095e26d26143e0568b789f5
Dinesh <87131578+AKRaavanan@users.noreply.github.com>
2022-02-04 18:25:31 +0530

    Add files via upload

---
+++ src/Pages/Explore/exploreComponent1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExploreComponent1 = () => {
+  return (
+  <div className='explore_body'>
+      <h3>All Collections</h3>
+  </div>
+  );
+};
+
+export default ExploreComponent1;
+
---
+++ src/Pages/Explore/nftspaceExplore.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NftspaceSidebar from '../Home/nftspace_Sidebar';
+import ExploreComponent1 from './exploreComponent1';
+
+const NftspaceExplore = () => {
+  return (
+    <div>   
+      <NftspaceSidebar />
+      <ExploreComponent1/>
+    </div>
+
+  );
+};
+
+export default NftspaceExplore;
